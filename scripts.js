f0d69fd66b4b26ae9155ab20ec61581f0c9cca50
unkelrara <unkelrara@yahoo.com>
2021-10-02 12:19:57 -0500

    single round complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-function
+let result = ["rock", "paper", "scissors"];
+function computerPlay() {
+    return result[Math.floor(Math.random()*result.length)];
+    
+}
+let computerResult = computerPlay();
+let playerInput = ("ScIsSoRs");
+function round() {
+    if (playerInput.toLowerCase() === computerResult) {
+        return["Tie!"];
+    } else if (playerInput.toLowerCase() === "rock" && computerResult === "scissors") {
+        return["Win!"];
+    } else if (playerInput.toLowerCase() === "paper" && computerResult === "rock") {
+        return["Win!"];
+    } else if (playerInput.toLowerCase() === "scissors" && computerResult === "paper") {
+        return["Win!"];
+    } else {
+        return["Lose!"];
+    }
+
+}
+console.log(round(), "player="+playerInput.toLowerCase(), "computer="+computerResult);
+
+
