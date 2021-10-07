3a125c37be60fb1b229af23f99a3848f01996466
unkelrara <unkelrara@yahoo.com>
2021-10-07 10:10:47 -0500

    rebuilt round, added 5 rounds with player input and a log of the winner of 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,24 +1,46 @@
 let result = ["rock", "paper", "scissors"];
 function computerPlay() {
-    return result[Math.floor(Math.random()*result.length)];
-    
+    return result[Math.floor(Math.random() * result.length)];
+
 }
-let computerResult = computerPlay();
-let playerInput = ("ScIsSoRs");
+let playerCount = 0;
+let computerCount = 0;
+let playerInput = "rock";/*prompt("Rock, paper or scissors?")*/
+let roundResult = round();
+let computerResult;
 function round() {
-    if (playerInput.toLowerCase() === computerResult) {
-        return["Tie!"];
-    } else if (playerInput.toLowerCase() === "rock" && computerResult === "scissors") {
-        return["Win!"];
-    } else if (playerInput.toLowerCase() === "paper" && computerResult === "rock") {
-        return["Win!"];
-    } else if (playerInput.toLowerCase() === "scissors" && computerResult === "paper") {
-        return["Win!"];
+    let playerInput = prompt("Rock, paper or scissors?");
+    let computerResult = computerPlay();
+    console.log("computer="+computerResult);
+    console.log("player="+playerInput);
+    if ((playerInput.toLowerCase() === "rock" && computerResult === "scissors") ||
+        (playerInput.toLowerCase() === "paper" && computerResult === "rock") ||
+        (playerInput.toLowerCase() === "scissors" && computerResult === "paper")) {
+        playerCount++
+        console.log("win");
+        return ["Win!"]
+    } else if (playerInput.toLowerCase() === computerResult) {
+        console.log("tie");
+        return ["tie!"]
     } else {
-        return["Lose!"];
+        computerCount++
+        console.log("lose");
+        return ["Lose!"]
     }
-
 }
-console.log(round(), "player="+playerInput.toLowerCase(), "computer="+computerResult);
+let counter = 0;
+do {
+    round();
+    counter++;
+    if (playerCount === 5){
+        console.log("You win!");
+    }
+    if (computerCount === 5){
+        console.log("You Lose! Good day sir!");
+    }
+} while ((playerCount < 5) && (computerCount < 5) && (counter < 20));
 
+/*console.log(roundResult, "player=" + playerInput.toLowerCase(), "computer=" + computerResult);*/
+console.log(playerCount);
+console.log(computerCount);
 
